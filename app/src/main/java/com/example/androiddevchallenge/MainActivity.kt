a236ecb252bb26bb6bde4333758732cbea58b77d
phc <675486916@qq.com>
2021-03-02 11:07:54 +0800

    .add adopt button

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -21,12 +21,24 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.Text
+import androidx.compose.material.Card
+import androidx.compose.material.Button
+import androidx.compose.material.SnackbarHostState
+import androidx.compose.material.SnackbarHost
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -37,6 +49,7 @@ import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.bean.Dog
 import com.example.androiddevchallenge.ui.theme.MyTheme
 import com.example.androiddevchallenge.vm.MainVH
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val mainVM by viewModels<MainVH>()
@@ -56,21 +69,20 @@ class MainActivity : AppCompatActivity() {
             super.onBackPressed()
         }
     }
+    val snackbarHostState = SnackbarHostState()
 
     // Start building your app here!
     @Composable
     fun MyApp() {
-
         Scaffold(modifier = Modifier.fillMaxWidth(),
             topBar = {
 
                 TopAppBar(
                     title = {
-                        Text("Select your favorite Dog!",color = Color.White)
+                        Text("Select your favorite Dog!", color = Color.White)
                     }
                 )
-            }) {
-
+            },snackbarHost = { SnackbarHost(hostState = snackbarHostState)}) {
             if (mainVM.curDog == null) {
                 DogList { dog -> onDogClick(dog) }
             } else {
@@ -102,16 +114,22 @@ class MainActivity : AppCompatActivity() {
 
     @Composable
     private fun DogDetail(dog: Dog) {
+        val scope = rememberCoroutineScope()
         Column {
             Image(
                 painter = painterResource(id = dog.logoId),
                 contentDescription = "Dog name is ${dog.name}",
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxWidth().height(300.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
             )
             Text(text = dog.name, color = Color.Black, style = MaterialTheme.typography.h4)
             Text(text = "age:${dog.age}", color = Color.Black)
             Text(text = dog.detail, color = Color.Yellow)
+            Button(onClick = { scope.launch { snackbarHostState.showSnackbar(message = "Adopt ${dog.name} success!") } }) {
+                Text(text = "Adopt")
+            }
         }
     }
 
@@ -131,6 +149,7 @@ class MainActivity : AppCompatActivity() {
                 Column {
                     Text(text = dog.name, color = Color.Black, style = MaterialTheme.typography.h4)
                     Text(text = dog.detail, color = Color.Yellow)
+
                 }
             }
         }
